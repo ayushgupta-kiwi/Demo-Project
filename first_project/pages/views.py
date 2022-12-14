7245a34bb37ea1062662c3e90c447f93e7afd585
ayushgupta-kiwi <ayush.gupta@kiwitech.com>
2022-12-14 15:24:59 +0530

    Python project

---
+++ first_project/pages/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import state_choices, bedroom_choices, price_choices
+
+from listings.models import Listing
+from realtors.models import Realtor
+
+
+def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+
+    return render(request, 'pages/index.html', context)
+
+
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
+
+
+def about(request):
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, 'pages/about.html', context)
